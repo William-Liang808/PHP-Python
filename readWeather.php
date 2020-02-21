c461b54739059333b625c3b33417ecd54663d916
SpicyWill <30877984+SpicyWill@users.noreply.github.com>
2020-02-21 09:17:13 -1000

    Update readWeather.php

--- readWeather.php
+++ readWeather.php
@@ -1,11 +1,18 @@
+<html>
+<body>
 <?php
 $servername = "localhost";
 $username = "root";
 $password = "raspberry";
 $dbname = "stem";
-
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+echo '<table border= "0" cellspacing="10" cellpadding="2">
+    <tr>    
+        <td> <font face ="Arial">Humidity</font> </td>
+        <td> <font face ="Arial">Temperature</font> </td>
+        <td> <font face ="Arial">Date/Time</font> </td>
+</tr>';
 // check connection
 if($conn->connect_error){
     die("connection failed:".$conn->connect_error);
@@ -17,13 +24,20 @@ $result =$conn->query($sql);
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo " humidity is: ".$row['humidity']."<br>". " temperature is: ".$row['temperature']."<br>". " date/time is: " . $row['dateTime']."<br><br>";
-    
+        $field1name = $row["humidity"];
+        $field2name = $row["temperature"];
+        $field3name = $row["dateTime"];
+ 
+        echo '<tr> 
+                  <td>'.$field1name.'</td> 
+                  <td>'.$field2name.'</td> 
+                  <td>'.$field3name.'</td> 
+              </tr>';
     }
 } else {
     echo "0 results";
 }
-
 $conn->close();
-
 ?>
+</body>
+</html>
