1230c8a81c6a71c8dfc51e9d078036d0d5bd9983
SpicyWill <30877984+SpicyWill@users.noreply.github.com>
2020-03-09 08:13:14 -1000

    Add files via upload

--- insertWeather.py
+++ insertWeather.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python
 import time
 import MySQLdb
+
 import Adafruit_DHT
 #Open database connection
 db =  MySQLdb.connect (host="localhost",user="root",passwd="raspberry",db="stem")
 #prep a cursor object using cursor() method; cursor is like a navigation method
 db.autocommit(True)
 cursor = db.cursor(MySQLdb.cursors.DictCursor)
-# Sensor should be set to Adafruit_DHT.DHT11
+# Sensor should be set to Adafruit_DHT.DHT11,
+# Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 sensor = Adafruit_DHT.DHT11
-# Using gpio pin 18
+
 pin = 18
+
+
 # Try to grab a sensor reading.  Use the read_retry method which will retry up
 # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-#Infinite loop that inserts values of humidity and temperature every 5 seconds
+
 while humidity !=0 and temperature !=0:
 	sql = f"INSERT INTO humidity (temperature, humidity) VALUES ({temperature}, {humidity})"
 	cursor.execute(sql) 
 	time.sleep(5)
-	
+
 cursor.close()
 db.close()
+
